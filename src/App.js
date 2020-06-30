5ff9a0a64efccebe3f78f252133c3cb00aeaca32
Erick-SN <ericksn0707@gmail.com>
2020-06-30 10:33:16 -0500

    Creation of the context for the modal and the Id of the challenge is obtained.

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Form from './components/Form';
 import List from './components/List';
 import CategoriesProvider from './context/CategoriesContext';
 import RecipesProvider from './context/RecipesContext';
+import ModalProvider from './context/ModalContext';
 function App() {
   return (
     <>
       <CategoriesProvider>
         <RecipesProvider>
-          <Header />
-          <div className='container mt-5'>
-            <div className='row'>
-              <Form />
+          <ModalProvider>
+            <Header />
+            <div className='container mt-5'>
+              <div className='row'>
+                <Form />
+              </div>
+              <List />
             </div>
-            <List />
-          </div>
+          </ModalProvider>
         </RecipesProvider>
       </CategoriesProvider>
     </>
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ModalContext } from '../context/ModalContext';
 const Recipe = ({ recipe }) => {
+  const { setIdRecipe } = useContext(ModalContext);
   return (
     <>
       <div className='col-md-4 mb-2'>
         <div className='card'>
           <h4 className='card-header text-center'>{recipe.strDrink}</h4>
           <div className='card-body'>
-            <button type='button' className='btn btn-block btn-primary'>
+            <button
+              type='button'
+              className='btn btn-block btn-primary'
+              onClick={() => {
+                setIdRecipe(recipe.idDrink);
+              }}>
               View
             </button>
           </div>
