1e57927582f763eed8ac6bd0942682c4135dcd4d
Erick-SN <ericksn0707@gmail.com>
2020-06-30 09:07:55 -0500

    using context into from component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { CategoriesContext } from '../context/CategoriesContext';
 const Form = () => {
+  const { categories } = useContext(CategoriesContext);
   return (
     <>
       <form className='col-12'>
@@ -19,6 +20,11 @@ const Form = () => {
           <div className='col-md-4'>
             <select className='form-control' name='category'>
               <option value=''>--Select--</option>
+              {categories.map((category) => (
+                <option key={category.strCategory} value={category.strCategory}>
+                  {category.strCategory}
+                </option>
+              ))}
             </select>
           </div>
           <div className='col-md-4'>
--- src/context/CategoriesContext.js
+++ src/context/CategoriesContext.js
@@ -1,10 +1,19 @@
-import React, { createContext, useState } from 'react';
-
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
 export const CategoriesContext = createContext();
 
 const CategoriesProvider = (props) => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const URL = `https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list`;
+      const categories = await axios.get(URL);
+      setCategories(categories.data.drinks);
+    };
+    getData();
+  }, []);
   return (
-    <CategoriesContext.Provider value={{}}>
+    <CategoriesContext.Provider value={{ categories }}>
       {props.children}
     </CategoriesContext.Provider>
   );
