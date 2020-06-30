e8765c0b82982bd1928a4baa9b57ef224c569d98
Erick-SN <ericksn0707@gmail.com>
2020-06-30 08:32:02 -0500

    Form component was created

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,33 @@ import React from 'react';
 const Form = () => {
   return (
     <>
-      <h1>Form</h1>
+      <form className='col-12'>
+        <fieldset className='text-center'>
+          <legend>Search Drinks</legend>
+        </fieldset>
+        <div className='row mt-4'>
+          <div className='col-md-4'>
+            <input
+              name='name'
+              className='form-control'
+              type='text'
+              placeholder='Search by ingredient'
+            />
+          </div>
+          <div className='col-md-4'>
+            <select className='form-control' name='category'>
+              <option value=''>--Select--</option>
+            </select>
+          </div>
+          <div className='col-md-4'>
+            <input
+              type='submit'
+              className='btn btn-block btn-primary'
+              value='Search Recipes'
+            />
+          </div>
+        </div>
+      </form>
     </>
   );
 };
