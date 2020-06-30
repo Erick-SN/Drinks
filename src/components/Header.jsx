ec3fcee082262a15bc76796ba6309ad80dd3aef9
Erick-SN <ericksn0707@gmail.com>
2020-06-30 08:18:50 -0500

    Project setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Header = () => {
+  return (
+    <>
+      <header className='bg-alert'>
+        <h1>Drink Recipes</h1>
+      </header>
+    </>
+  );
+};
+
+export default Header;
